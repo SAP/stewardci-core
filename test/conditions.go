f788cff979d0eb502ee08ad151c60ec36dc871a5
Matthias Rinck <m.rinck@sap.com>
2019-11-08 13:35:10 +0000

    Add cleanup for tenant test

--- test/conditions.go
+++ test/conditions.go
@@ -1,31 +1,32 @@
 package test
 
 import (
-    "fmt"
-
-api "github.com/SAP/stewardci-core/pkg/apis/steward/v1alpha1"
-"github.com/SAP/stewardci-core/pkg/k8s"
+	"github.com/SAP/stewardci-core/pkg/k8s"
 )
 
+type WaitConditionFunc func(k8s.ClientFactory) (bool, error)
 
-type WaitCondition func(k8s.ClientFactory) (bool, error)
-
-type TenantStateCheck func(*api.Tenant) (bool) 
-
-func CreateTenantCondition(tenant *api.Tenant, check TenantStateCheck) WaitCondition {
-  return func (clientFactory k8s.ClientFactory) (bool, error) {
-      fetcher := k8s.NewTenantFetcher(clientFactory)
-      key := fmt.Sprintf("%s/%s",tenant.GetNamespace(),tenant.GetName())
-      tenant,err := fetcher.ByKey(key)
-      if err != nil {
-          return true,err
-      }
-      return check(tenant),nil
-}
+type waitCondition struct {
+	conditionFunc WaitConditionFunc
+	name          string
 }
 
-func TenantHasStateResult(result api.TenantResult) TenantStateCheck {
-    return  func(tenant *api.Tenant) (bool) {
-   return tenant.Status.Result == result
+type WaitCondition interface {
+	Wait(k8s.ClientFactory) (bool, error)
+	Name() string
 }
-} 
+
+func NewWaitCondition(f WaitConditionFunc, name string) WaitCondition {
+	return &waitCondition{
+		conditionFunc: f,
+		name:          name,
+	}
+}
+
+func (w *waitCondition) Wait(clientFactory k8s.ClientFactory) (bool, error) {
+	return w.conditionFunc(clientFactory)
+}
+
+func (w *waitCondition) Name() string {
+	return w.name
+}
