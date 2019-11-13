24348ce85a990c731e43c30938bc1930ef02d7e5
Matthias Rinck <m.rinck@sap.com>
2019-11-13 09:09:42 +0000

    Add test for pipeline condition

---
+++ test/pipelineRun_conditions_test.go
@@ -0,0 +1,42 @@
+package test
+
+import (
+	"testing"
+        "time"
+	api "github.com/SAP/stewardci-core/pkg/apis/steward/v1alpha1"
+	"github.com/SAP/stewardci-core/pkg/k8s"
+	"github.com/SAP/stewardci-core/pkg/k8s/fake"
+	"github.com/SAP/stewardci-core/test/builder"
+	"gotest.tools/assert"
+)
+
+func Test_PipelineCondition(t *testing.T) {
+	check := PipelineRunHasStateResult(api.ResultSuccess)
+	pipelineRun :=
+		builder.PipelineRun("namespace1")
+	clientFactory := fake.NewClientFactory()
+	waiter := NewWaiter(clientFactory)
+	pr, err := createPipelineRun(clientFactory, pipelineRun)
+	assert.NilError(t, err)
+	pipelineRunCheck := CreatePipelineRunCondition(pr, check, "Test")
+	errorChan := make(chan error)
+
+	go func() {
+		errorChan <- waiter.WaitFor(pipelineRunCheck)
+	}()
+        time.Sleep(3 * time.Second)
+	setState(clientFactory, pr, api.ResultSuccess)
+	err = <-errorChan
+	assert.NilError(t, err)
+}
+
+func setState(clientFactory k8s.ClientFactory, pipelineRun *api.PipelineRun, result api.Result) {
+	fetcher := k8s.NewPipelineRunFetcher(clientFactory)
+	pr, _ := fetcher.ByName(pipelineRun.GetNamespace(), pipelineRun.GetName())
+	pr.UpdateResult(result)
+}
+
+func createPipelineRun(clientFactory k8s.ClientFactory, pipelineRun *api.PipelineRun) (*api.PipelineRun, error) {
+	stewardClient := clientFactory.StewardV1alpha1().PipelineRuns(pipelineRun.GetNamespace())
+	return stewardClient.Create(pipelineRun)
+}
