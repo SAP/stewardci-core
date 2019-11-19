06495897c8c54761612c46af3c490c01bbaa5399
Andreas Brehmer <andreas.brehmer@sap.com>
2019-11-19 16:00:24 +0100

    fix+improve tests

--- pkg/k8s/secrets/secretUtils_test.go
+++ pkg/k8s/secrets/secretUtils_test.go
@@ -2,12 +2,13 @@ package secrets
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 	"testing"
 
 	"github.com/SAP/stewardci-core/pkg/k8s/fake"
 	secretMocks "github.com/SAP/stewardci-core/pkg/k8s/secrets/mocks"
-	provider "github.com/SAP/stewardci-core/pkg/k8s/secrets/providers/fake"
+	fakesecretprovider "github.com/SAP/stewardci-core/pkg/k8s/secrets/providers/fake"
 	gomock "github.com/golang/mock/gomock"
 	"github.com/pkg/errors"
 	"gotest.tools/assert"
@@ -22,150 +23,201 @@ const (
 	targetNamespace = "targetNs"
 )
 
-func initHelperWithMock(t *testing.T, secrets ...*v1.Secret) (SecretHelper, *secretMocks.MockSecretHelper) {
+func initSecretHelperWithMock(
+	t *testing.T, mockCtrl *gomock.Controller, secrets ...*v1.Secret,
+) (
+	SecretHelper, *secretMocks.MockSecretHelper,
+) {
 	t.Helper()
-	provider := provider.NewProvider(namespace, secrets...)
+	fakeSecretProvider := fakesecretprovider.NewProvider(namespace, secrets...)
 
 	cf := fake.NewClientFactory()
 	targetClient := cf.CoreV1().Secrets(targetNamespace)
 
-	mockCtrl := gomock.NewController(t)
 	mockSecretHelper := secretMocks.NewMockSecretHelper(mockCtrl)
-	helper := NewSecretHelper(provider, targetNamespace, targetClient).(*secretHelper)
-	helper.testing = &secretHelperTesting{createSecretStub: mockSecretHelper.CreateSecret}
+	helper := NewSecretHelper(fakeSecretProvider, targetNamespace, targetClient).(*secretHelper)
+	helper.testing = &secretHelperTesting{
+		createSecretStub: mockSecretHelper.CreateSecret,
+	}
 	return helper, mockSecretHelper
 }
 
 func Test_CopySecrets_NoFilter(t *testing.T) {
 	t.Parallel()
-	// SETUP
-	secret := fake.SecretOpaque("foo", namespace)
-	helper, mockSecretHelper := initHelperWithMock(t, secret)
-	expectedSecret := fake.SecretOpaque("foo", "")
-	// EXPECT
-	mockSecretHelper.EXPECT().CreateSecret(expectedSecret).Return(expectedSecret, nil)
-	// EXERCISE
-	list, err := helper.CopySecrets([]string{"foo"}, nil)
-	// VERIFY
-	assert.NilError(t, err)
-	assert.DeepEqual(t, []string{"foo"}, list)
-}
 
-func nameStartsWithB(secret *v1.Secret) bool {
-	return strings.HasPrefix(secret.GetName(), "b")
+	// SETUP
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	secret := fake.SecretOpaque("foo", namespace)
+	examinee, mockSecretHelper := initSecretHelperWithMock(t, mockCtrl, secret)
+
+	// EXPECT
+	expectedSecret := fake.SecretOpaque("foo", "")
+	mockSecretHelper.EXPECT().CreateSecret(expectedSecret).Return(expectedSecret, nil)
+
+	// EXERCISE
+	resultList, resultErr := examinee.CopySecrets([]string{"foo"}, nil)
+
+	// VERIFY
+	assert.NilError(t, resultErr)
+	assert.DeepEqual(t, []string{"foo"}, resultList)
 }
 
 func Test_CopySecrets_WithFilter(t *testing.T) {
 	t.Parallel()
+
 	// SETUP
-	helper, mockSecretHelper := initHelperWithMock(t,
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	examinee, mockSecretHelper := initSecretHelperWithMock(t, mockCtrl,
 		fake.SecretOpaque("foo", namespace),
 		fake.SecretOpaque("bar", namespace),
+		fake.SecretOpaque("foo2", namespace),
 		fake.SecretOpaque("baz", namespace),
+		fake.SecretOpaque("foo3", namespace),
 	)
-	expectedSecret1 := fake.SecretOpaque("foo", "")
+
+	filter := func(secret *v1.Secret) bool {
+		return strings.HasPrefix(secret.GetName(), "b")
+	}
+
+	// EXPECT
 	expectedSecret2 := fake.SecretOpaque("bar", "")
-	expectedSecret3 := fake.SecretOpaque("baz", "")
-	// VERIFY
-	mockSecretHelper.EXPECT().CreateSecret(expectedSecret1).Return(expectedSecret1, nil)
 	mockSecretHelper.EXPECT().CreateSecret(expectedSecret2).Return(expectedSecret2, nil)
+	expectedSecret3 := fake.SecretOpaque("baz", "")
 	mockSecretHelper.EXPECT().CreateSecret(expectedSecret3).Return(expectedSecret3, nil)
+
 	// EXERCISE
-	list, err := helper.CopySecrets([]string{"foo", "bar", "baz"}, nameStartsWithB)
+	resultList, resultErr := examinee.CopySecrets(
+		[]string{"foo", "bar", "foo2", "baz", "foo3"},
+		filter,
+	)
+
 	// VERIFY
-	assert.NilError(t, err)
-	assert.DeepEqual(t, []string{"bar", "baz"}, list)
+	assert.NilError(t, resultErr)
+	assert.DeepEqual(t, []string{"bar", "baz"}, resultList)
 }
 
 func Test_CopySecrets_NotExisting(t *testing.T) {
 	t.Parallel()
+
 	// SETUP
-	helper, mockSecretHelper := initHelperWithMock(t,
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	examinee, mockSecretHelper := initSecretHelperWithMock(t, mockCtrl,
 		fake.SecretOpaque("foo", namespace),
 		fake.SecretOpaque("bar", namespace),
 	)
+
+	// EXPECT
 	expectedSecret := fake.SecretOpaque("foo", "")
-	// VERIFY
 	mockSecretHelper.EXPECT().CreateSecret(expectedSecret).Return(expectedSecret, nil)
+
 	// EXERCISE
-	list, err := helper.CopySecrets([]string{"foo", "notExistingSecret1", "bar"}, nil)
+	resultList, resultErr := examinee.CopySecrets(
+		[]string{"foo", "notExistingSecret1", "bar"},
+		nil,
+	)
+
 	// VERIFY
-	assert.Assert(t, helper.IsNotFound(err))
-	assert.DeepEqual(t, []string{"foo"}, list)
+	assert.Assert(t, examinee.IsNotFound(resultErr))
+	assert.DeepEqual(t, []string{"foo"}, resultList)
 }
 
-func initHelperWithClient(secrets ...*v1.Secret) (SecretHelper, corev1.SecretInterface) {
-	provider := provider.NewProvider(namespace, secrets...)
+func initSecretHelperWithClient(secrets ...*v1.Secret) (SecretHelper, corev1.SecretInterface) {
+	provider := fakesecretprovider.NewProvider(namespace, secrets...)
 	cf := fake.NewClientFactory()
 	targetClient := cf.CoreV1().Secrets(targetNamespace)
 	return NewSecretHelper(provider, targetNamespace, targetClient), targetClient
 }
 
-func Test_CreateSecret(t *testing.T) {
+func Test_CreateSecret_GoodCase(t *testing.T) {
 	t.Parallel()
+
 	// SETUP
-	helper, targetClient := initHelperWithClient()
-	secret := fake.SecretOpaque("foo", namespace)
+	examinee, targetClient := initSecretHelperWithClient()
+	origSecret := fake.SecretOpaque("foo", namespace)
+
 	// EXERCISE
-	_, err := helper.CreateSecret(secret)
+	resultSecret, resultErr := examinee.CreateSecret(origSecret.DeepCopy())
+
 	// VERIFY
-	assert.NilError(t, err)
+	expectedSecret := origSecret.DeepCopy()
+	expectedSecret.SetNamespace(targetNamespace)
+
+	assert.NilError(t, resultErr)
+	assert.DeepEqual(t, expectedSecret, resultSecret)
 	storedSecret, err := targetClient.Get("foo", metav1.GetOptions{})
 	assert.NilError(t, err)
-	assert.Equal(t, "foo", storedSecret.GetName())
+	assert.DeepEqual(t, expectedSecret, storedSecret)
 }
 
 func Test_CreateSecret_Error(t *testing.T) {
 	t.Parallel()
+
 	// SETUP
-	provider := provider.NewProvider(namespace)
+	fakeSecretProvider := fakesecretprovider.NewProvider(namespace)
 	cs := kubernetes.NewSimpleClientset()
 	expectedError := fmt.Errorf("expected")
 	cs.PrependReactor("create", "*", fake.NewErrorReactor(expectedError))
-	helper := NewSecretHelper(provider, targetNamespace, cs.CoreV1().Secrets(targetNamespace))
-	secret := fake.SecretOpaque("foo", namespace)
+
+	origSecret := fake.SecretOpaque("foo", namespace)
+	examinee := NewSecretHelper(fakeSecretProvider, targetNamespace, cs.CoreV1().Secrets(targetNamespace))
 
 	// EXERCISE
-	_, err := helper.CreateSecret(secret)
+	resultSecret, resultErr := examinee.CreateSecret(origSecret.DeepCopy())
 
 	// VERIFY
-	assert.Assert(t, expectedError == err)
+	assert.Assert(t, expectedError == resultErr)
+	assert.Assert(t, resultSecret == nil)
 }
 
 func Test_IsNotFound_True(t *testing.T) {
 	t.Parallel()
-	helper, _ := initHelperWithClient()
-	err := NewNotFoundError("foo")
-	assert.Assert(t, helper.IsNotFound(err))
+
+	for ti, notFoundError := range []error{
+		// direct
+		NewNotFoundError("foo"),
+		// cause
+		errors.Wrap(NewNotFoundError("foo"), "bar"),
+		// cause of cause
+		errors.Wrap(errors.Wrap(NewNotFoundError("foo"), "bar"), "baz"),
+		// cause
+		errors.WithMessage(NewNotFoundError("foo"), "bar"),
+	} {
+		t.Run(strconv.Itoa(ti), func(t *testing.T) {
+			// SETUP
+			examinee, _ := initSecretHelperWithClient()
+
+			// EXERCISE
+			result := examinee.IsNotFound(notFoundError)
+
+			// VERIFY
+			assert.Assert(t, result == true)
+		})
+	}
 }
 
-func Test_IsNotFound_Wrapped(t *testing.T) {
+func Test_IsNotFound_False(t *testing.T) {
 	t.Parallel()
-	helper, _ := initHelperWithClient()
-	err := NewNotFoundError("foo")
-	err = errors.Wrap(err, "failed to copy secrets")
-	assert.Assert(t, helper.IsNotFound(err))
-}
 
-func Test_IsNotFound_SameText(t *testing.T) {
-	t.Parallel()
-	helper, _ := initHelperWithClient()
-	err := NewNotFoundError("foo")
-	err = errors.WithMessage(nil, err.Error())
-	assert.Assert(t, false == helper.IsNotFound(err))
-}
+	for ti, otherError := range []error{
+		// same message as NFE:
+		errors.New(NewNotFoundError("foo").Error()),
+		fmt.Errorf("foo"),
+	} {
+		t.Run(strconv.Itoa(ti), func(t *testing.T) {
+			// SETUP
+			examinee, _ := initSecretHelperWithClient()
 
-func Test_IsNotFound_FmtError(t *testing.T) {
-	t.Parallel()
-	helper, _ := initHelperWithClient()
-	err := fmt.Errorf("foo")
-	assert.Assert(t, false == helper.IsNotFound(err))
-}
+			// EXERCISE
+			result := examinee.IsNotFound(otherError)
 
-func Test_IsNotFound_WithMessage(t *testing.T) {
-	t.Parallel()
-	helper, _ := initHelperWithClient()
-	err := NewNotFoundError("foo")
-	err = errors.WithMessage(err, "baz")
-	assert.Assert(t, helper.IsNotFound(err))
+			// VERIFY
+			assert.Assert(t, result == false)
+		})
+	}
 }
