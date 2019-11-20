ed22681a4fa9e94c7da6bc2f444a611c97e69551
Matthias Rinck <m.rinck@sap.com>
2019-11-20 12:17:36 +0000

    extend pipelineRun builder

--- test/builder/pipelineRun_test.go
+++ test/builder/pipelineRun_test.go
@@ -3,6 +3,8 @@ package builder
 import (
 	"gotest.tools/assert"
 	"testing"
+
+	api "github.com/SAP/stewardci-core/pkg/apis/steward/v1alpha1"
 )
 
 func Test_PipelineRunBuilder_Jenkinsfile(t *testing.T) {
@@ -18,7 +20,31 @@ func Test_PipelineRunBuilder_ArgSpec(t *testing.T) {
 	pipelineRun := PipelineRun("prefix1", "namespace1",
 		PipelineRunSpec(
 			ArgSpec("foo", "bar"),
-			ArgSpec("baz", "bum")))
-	assert.Equal(t, "bar", pipelineRun.Spec.Args["foo"])
-	assert.Equal(t, "bum", pipelineRun.Spec.Args["baz"])
+			ArgSpec("baz", "bum"),
+		),
+	)
+	assert.DeepEqual(t, map[string]string{"foo": "bar", "baz": "bum"}, pipelineRun.Spec.Args)
+}
+
+func Test_PipelineRunBuilder_Secret(t *testing.T) {
+	pipelineRun := PipelineRun("prefix1", "namespace1",
+		PipelineRunSpec(
+			Secret("foo"),
+			Secret("bar"),
+		),
+	)
+	assert.DeepEqual(t, []string{"foo", "bar"}, pipelineRun.Spec.Secrets)
+}
+
+func Test_PipelineRunBuilder_RunDetails(t *testing.T) {
+	pipelineRun := PipelineRun("prefix1", "namespace1",
+		PipelineRunSpec(
+			RunDetails("jobName1", "cause1", 42),
+		),
+	)
+	assert.DeepEqual(t, &api.PipelineRunDetails{
+		JobName:        "jobName1",
+		SequenceNumber: 42,
+		Cause:          "cause1",
+	}, pipelineRun.Spec.RunDetails)
 }
