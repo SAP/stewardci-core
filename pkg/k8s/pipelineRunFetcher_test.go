e93540c2ca2b9cd190f907847400dd93857d22af
Matthias Rinck <m.rinck@sap.com>
2019-12-06 11:26:42 +0100

    add tests for lister based pipelinerun fetcher

--- pkg/k8s/pipelineRunFetcher_test.go
+++ pkg/k8s/pipelineRunFetcher_test.go
@@ -3,11 +3,14 @@ package k8s
 import (
 	"testing"
 
+	api "github.com/SAP/stewardci-core/pkg/apis/steward/v1alpha1"
+	stewardLister "github.com/SAP/stewardci-core/pkg/client/listers/steward/v1alpha1"
 	"github.com/SAP/stewardci-core/pkg/k8s/fake"
 	"gotest.tools/assert"
+	"k8s.io/client-go/tools/cache"
 )
 
-func Test_pipelineRunFetcher_ByName_NotExisting(t *testing.T) {
+func Test_ClientBasedPipelineRunFetcher_ByName_NotExisting(t *testing.T) {
 	// SETUP
 	factory := fake.NewClientFactory()
 	client := factory.StewardV1alpha1()
@@ -21,7 +24,7 @@ func Test_pipelineRunFetcher_ByName_NotExisting(t *testing.T) {
 	assert.NilError(t, resultErr)
 }
 
-func Test_pipelineRunFetcher_ByName_GoodCase(t *testing.T) {
+func Test_ClientBasedPipelineRunFetcher_ByName_GoodCase(t *testing.T) {
 	// SETUP
 	const (
 		secretName = "secret1"
@@ -39,7 +42,7 @@ func Test_pipelineRunFetcher_ByName_GoodCase(t *testing.T) {
 	assert.DeepEqual(t, run, resultObj)
 }
 
-func Test_pipelineRunFetcher_ByKey_GoodCase(t *testing.T) {
+func Test_ClientBasedPipelineRunFetcher_ByKey_GoodCase(t *testing.T) {
 	// SETUP
 	const (
 		secretName = "secret1"
@@ -56,4 +59,60 @@ func Test_pipelineRunFetcher_ByKey_GoodCase(t *testing.T) {
 	// VERIFY
 	assert.NilError(t, resultErr)
 	assert.DeepEqual(t, run, resultObj)
+}
+
+func Test_ListerBasedPipelineRunFetcher_ByName_NotExisting(t *testing.T) {
+	// SETUP
+	lister := createLister()
+	examinee := NewListerBasedPipelineRunFetcher(lister)
+
+	// EXERCISE
+	pipelineRun, resultErr := examinee.ByName(ns1, "NotExisting1")
+
+	// VERIFY
+	assert.Assert(t, pipelineRun == nil)
+	assert.NilError(t, resultErr)
+}
+
+func Test_ListerBasedPipelineRunFetcher_ByName_GoodCase(t *testing.T) {
+	// SETUP
+	const (
+		secretName = "secret1"
+	)
+	run := newPipelineRunWithSecret(ns1, run1, secretName)
+	lister := createLister(run)
+	examinee := NewListerBasedPipelineRunFetcher(lister)
+	// EXERCISE
+	resultObj, resultErr := examinee.ByName(ns1, run1)
+
+	// VERIFY
+	assert.NilError(t, resultErr)
+	assert.DeepEqual(t, run, resultObj)
+}
+
+func Test_ListerBasedPipelineRunFetcher_ByKey_GoodCase(t *testing.T) {
+	// SETUP
+	const (
+		secretName = "secret1"
+	)
+	run := newPipelineRunWithSecret(ns1, run1, secretName)
+	lister := createLister(run)
+	examinee := NewListerBasedPipelineRunFetcher(lister)
+	key := fake.ObjectKey(run1, ns1)
+
+	// EXERCISE
+	resultObj, resultErr := examinee.ByKey(key)
+
+	// VERIFY
+	assert.NilError(t, resultErr)
+	assert.DeepEqual(t, run, resultObj)
+}
+
+func createLister(runs ...*api.PipelineRun) stewardLister.PipelineRunLister {
+	indexer := cache.NewIndexer(cache.MetaNamespaceKeyFunc,
+		cache.Indexers{cache.NamespaceIndex: cache.MetaNamespaceIndexFunc})
+	for _, run := range runs {
+		indexer.Add(run)
+	}
+	return stewardLister.NewPipelineRunLister(indexer)
 }
