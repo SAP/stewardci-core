2df24a31bb1f746cc554499ca783f400ff65bba9
Andreas Brehmer <andreas.brehmer@sap.com>
2020-02-13 12:23:53 +0100

    enforce unprivileged pipeline run containers
    - chart: tighten the pod security policy
    - chart: configure UID/GID/fsGroup for JFR pod
    - runctl: set UID/GID/fsGroup for JRF pod

--- pkg/runctl/config.go
+++ pkg/runctl/config.go
@@ -1,8 +1,11 @@
 package runctl
 
 import (
+	"strconv"
+
 	"github.com/SAP/stewardci-core/pkg/k8s"
 
+	"github.com/pkg/errors"
 	k8serrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"knative.dev/pkg/system"
@@ -11,10 +14,16 @@ import (
 const (
 	pipelineRunsConfigMapName          = "steward-pipelineruns"
 	pipelineRunsConfigKeyNetworkPolicy = "networkPolicy"
+	pipelineRunsConfigKeyPSCRunAsUser  = "jenkinsfileRunner.podSecurityContext.runAsUser"
+	pipelineRunsConfigKeyPSCRunAsGroup = "jenkinsfileRunner.podSecurityContext.runAsGroup"
+	pipelineRunsConfigKeyPSCFSGroup    = "jenkinsfileRunner.podSecurityContext.fsGroup"
 )
 
 type pipelineRunsConfigStruct struct {
-	NetworkPolicy string
+	NetworkPolicy                                 string
+	JenkinsfileRunnerPodSecurityContextRunAsUser  *int64
+	JenkinsfileRunnerPodSecurityContextRunAsGroup *int64
+	JenkinsfileRunnerPodSecurityContextFSGroup    *int64
 }
 
 func loadPipelineRunsConfig(clientFactory k8s.ClientFactory) (*pipelineRunsConfigStruct, error) {
@@ -29,6 +38,31 @@ func loadPipelineRunsConfig(clientFactory k8s.ClientFactory) (*pipelineRunsConfi
 
 	config := &pipelineRunsConfigStruct{
 		NetworkPolicy: configMap.Data[pipelineRunsConfigKeyNetworkPolicy],
+	}
+
+	parseInt64 := func(key string) (*int64, error) {
+		if strVal, ok := configMap.Data[key]; ok && strVal != "" {
+			intVal, err := strconv.ParseInt(strVal, 10, 64)
+			if err != nil {
+				return nil, errors.Wrapf(err, "cannot parse configuration value %q", key)
+			}
+			return &intVal, nil
+		}
+		return nil, nil
+	}
+
+	if config.JenkinsfileRunnerPodSecurityContextRunAsUser, err =
+		parseInt64(pipelineRunsConfigKeyPSCRunAsUser); err != nil {
+		return nil, err
+	}
+	if config.JenkinsfileRunnerPodSecurityContextRunAsGroup, err =
+		parseInt64(pipelineRunsConfigKeyPSCRunAsGroup); err != nil {
+		return nil, err
+	}
+
+	if config.JenkinsfileRunnerPodSecurityContextFSGroup, err =
+		parseInt64(pipelineRunsConfigKeyPSCFSGroup); err != nil {
+		return nil, err
 	}
 
 	return config, nil
