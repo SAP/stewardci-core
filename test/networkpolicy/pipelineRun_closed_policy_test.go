e84ce8ef9fe4e1a132a0fe97ab723964c1464474
Matthias Rinck <m.rinck@sap.com>
2020-02-18 08:53:06 -0800

    Add integration tests for network policy

---
+++ test/networkpolicy/pipelineRun_closed_policy_test.go
@@ -0,0 +1,17 @@
+// +build closednet
+
+package networkpolicy
+
+import (
+	"testing"
+
+	f "github.com/SAP/stewardci-core/test/framework"
+	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
+)
+
+func Test_PipelineRunClosedNetworkPolicy(t *testing.T) {
+    npTest := f.TestPlan{TestBuilder: PipelineRunNetworkClosedPolicy,
+                Count:         1,
+        }
+        f.ExecutePipelineRunTests(t, npTest)
+}
