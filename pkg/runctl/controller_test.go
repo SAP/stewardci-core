a6904d0d6746ae87f0e26244ee72d8f9d9423a09
Matthias Rinck <56343232+rinckm@users.noreply.github.com>
2020-06-29 16:00:44 +0200

    Update pkg/runctl/controller_test.go
    Co-authored-by: Andreas Brehmer <38314220+anbrsap@users.noreply.github.com>

--- pkg/runctl/controller_test.go
+++ pkg/runctl/controller_test.go
@@ -267,7 +267,7 @@ func Test_Controller_syncHandler_mock(t *testing.T) {
 			runManagerExpectation: func(rm *runmocks.MockManager, run *runmocks.MockRun) {
 				rm.EXPECT().Start(gomock.Any()).Return(expectedError)
 			},
-			expectedResult:  "",
+			expectedResult:  api.ResultUndefined,
 			expectedState:   api.StatePreparing,
 			expectedMessage: "error syncing resource .*expected",
 			expectedError:   expectedError,
