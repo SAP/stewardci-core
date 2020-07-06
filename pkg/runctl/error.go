77966ae1088b96230fa721078ba2f612f905e4ae
Matthias Rinck <m.rinck@sap.com>
2020-07-06 07:49:44 -0700

    Rework error

--- pkg/runctl/error.go
+++ pkg/runctl/error.go
@@ -4,35 +4,35 @@ import (
 	"errors"
 )
 
-// Error is a runManager error
-type Error struct {
-	e           error
+type recoverableInfoError struct {
+	err         error
 	recoverable bool
 }
 
 // RecoverabilityInfo is exposed by errors that can be converted to an RecoverabilityInfo object
 type RecoverabilityInfo interface {
+        error 
 	IsRecoverable() bool
 }
 
 // NewRecoverabilityInfoError creates a new error implementing the RecoverabilityInfo interface
-func NewRecoverabilityInfoError(err error, recoverable bool) *Error {
+func NewRecoverabilityInfoError(err error, recoverable bool) RecoverabilityInfo {
 	if err == nil {
 		panic("Cannot use nil as error")
 	}
 
-	return &Error{e: err,
+	return &recoverableInfoError{err: err,
 		recoverable: recoverable,
 	}
 }
 
 // Error returns error string of the provided error
-func (err *Error) Error() string {
-	return err.e.Error()
+func (err *recoverableInfoError) Error() string {
+	return err.err.Error()
 }
 
 // IsRecoverable returns true if error can be recovered from
-func (err *Error) IsRecoverable() bool {
+func (err *recoverableInfoError) IsRecoverable() bool {
 	if err == nil {
 		return false
 	}
