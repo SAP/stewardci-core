387671111d298e2b0136bac9f35beeb95902ddf4
Andreas Brehmer <andreas.brehmer@sap.com>
2021-12-06 10:44:26 +0100

    Fix errors introduced by #290

---
+++ test/shared/constants.go
@@ -0,0 +1,11 @@
+package shared
+
+const (
+	// ExamplePipelineRepoURL is the URL of a Git repository with example
+	// Steward pipeline run manifests.
+	ExamplePipelineRepoURL = "https://github.com/SAP-samples/stewardci-example-pipelines"
+
+	// ExamplePipelineRepoRevision is the revision of ExamplePipelineRepoURL
+	// to be used by tests.
+	ExamplePipelineRepoRevision = "main"
+)
