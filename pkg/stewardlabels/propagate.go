5aea40b0723671be8f0a0073e3bd93b3edb0bb16
Matthias Rinck <m.rinck@sap.com>
2023-03-09 10:23:41 +0100

    fix lint error

--- pkg/stewardlabels/propagate.go
+++ pkg/stewardlabels/propagate.go
@@ -48,7 +48,7 @@ func propagate(destObj metav1.Object, sourceObj metav1.Object, labelSpec map[str
 	destLabels := destObj.GetLabels()
 
 	// propagate
-	err, propagatedLabels := getPropagatedLabels(labelSpec, sourceLabels, destLabels)
+	propagatedLabels, err := getPropagatedLabels(labelSpec, sourceLabels, destLabels)
 	if err != nil {
 		return err
 	}
@@ -88,7 +88,7 @@ func checkConflict(labelSpec, sourceLabels map[string]string) error {
 	return nil
 }
 
-func getPropagatedLabels(labelSpec, sourceLabels, destLabels map[string]string) (error, map[string]string) {
+func getPropagatedLabels(labelSpec, sourceLabels, destLabels map[string]string) (map[string]string, error) {
 	// don't modify labels of dest object until propagation finished without errors
 	propagatedLabels := make(map[string]string)
 
@@ -104,14 +104,14 @@ func getPropagatedLabels(labelSpec, sourceLabels, destLabels map[string]string) 
 		destValue, found := destLabels[k]
 		if found {
 			if destValue != v {
-				return fmt.Errorf(
+				return nil, fmt.Errorf(
 					"value conflict: destination object label %q has existing value %q but %q is expected",
 					k, destValue, v,
-				), nil
+				)
 			}
 		} else {
 			propagatedLabels[k] = v
 		}
 	}
-	return nil, propagatedLabels
+	return propagatedLabels, nil
 }
